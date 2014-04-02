62e7f9362c4e5719a5c70cab9cb5d72fc3dfa9de
Alice <alice.mottola@gmail.com>
2014-04-02 17:12:07 -0400

    Changes trie_builder to dictionary and modifies outline

---
+++ lib/dictionary.rb
@@ -0,0 +1,18 @@
+require 'trie'
+
+module Dictionary
+
+	extend self
+
+	def populate(filepath)
+	end
+
+	def match_words(prefix)
+	end
+
+private
+
+	def trie_from_system_dictionary(filepath)
+	end
+
+end
